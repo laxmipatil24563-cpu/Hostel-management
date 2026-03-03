de1b15186847746ff0311623ece3678985669473
laxmipatil24563-cpu <laxmipatil24563@gmail.com>
2026-03-03 11:10:36 +0530

    Add files via upload

---
+++ autoboxdemo.java
@@ -0,0 +1,10 @@
+class autoboxdemo
+{
+ public static void main(String a[])
+ {
+  int A=10;
+  Integer i=A;
+  System.out.println("Primitive A is : "+A);
+  System.out.println("Wrapper Class Object i is : "+i);
+ }
+}
